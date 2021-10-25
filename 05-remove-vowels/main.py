1577158ad3337c72517301b67a9791685311118c
Pascal Salvador <smv7.github@gmail.com>
2021-10-25 13:21:47 -0300

    Removing debug code and improving program usability

--- 05-remove-vowels/main.py
+++ 05-remove-vowels/main.py
@@ -1,9 +1,24 @@
 # This Python program remove all the vowels of the text passed 
 # by argument to the program. Idea based on the hebrew language.
 
+from modules import remove_vowels
+
 
 def run():
-    pass
+    welcome_message = open('./program_messages/welcome.txt', 'r')
+    print(welcome_message.read())
+
+    while True:
+        file_path = input('Enter the file path: ')
+        file_path = file_path.strip()
+        try:
+            remove_vowels(file_path)
+        except:
+            print('\nError, the path you entered do not exist.\nTry again\n')
+            continue
+        break
+
+    print('\nAll done.\n')
 
 
 if __name__ == '__main__':
--- 05-remove-vowels/modules.py
+++ 05-remove-vowels/modules.py
@@ -1,22 +1,19 @@
 import re
 
 
-def stop():
-    user_answer = input('Enter to continue ')
-
-
 def remove_vowels(file_path):
     '''Function to create a new file without vowels from a text file passed by argument.'''
-
-    print('\nStarting to create the file...\n')
 
     file_object = open(file_path, 'r')
     reading_file = file_object.readlines()
     vowels_regex = '[aeiouAEIOU]'
 
-    file_without_vowels = open('without_vowels.txt', 'w')
+    file_without_vowels_name = input('Name of the new file: ')
+
+    file_without_vowels = open('./files_without_vowels/{}.txt'.format(file_without_vowels_name), 'w')
     
-    # print(reading_file)
+    print('\nStarting to create the file...')
+
     for line in reading_file:
         # line = line.strip()
         words = line.split()
@@ -26,28 +23,16 @@ def remove_vowels(file_path):
         for word in words:
             word_without_vowels = ''
             index = 0
+
             while index < len(word):
                 if not re.match(vowels_regex, word[index]):
                     word_without_vowels += word[index]
-                    # print('Regex match!\nword_without_vowels variable is: {}\n'.format(word_without_vowels))
-                    # stop()
                 index += 1
-            line_without_vowels.append(word_without_vowels)
-            # print('Ending word iteration!\nline_without_vowels variable is now: {}\n'.format(line_without_vowels))
-            # stop()
 
+            line_without_vowels.append(word_without_vowels)
+
+        line_without_vowels.append('\n')
         new_line = ' '.join(line_without_vowels)
         file_without_vowels.write(new_line)
-        
-        # print('Exit word array processing loop!\new_line variable is: {}\n'.format(new_line))
-        # print('Proceding to write new line:\n\t"{}"\n'.format(new_line))
-        # stop()
 
-    print('\nThe file was successfully created.\n')
-        
-
-history = './history.txt'
-remove_vowels(history)
-new_file_object = open('./without_vowels.txt', 'r')
-# reading_file_without_vowels = new_file_object.read()
-print(new_file_object.read())
+    print('The file was successfully created!\n')
