e0107ab0d26b753e66a8c605861bf0904245c1c8
Pascal Salvador <smv7.github@gmail.com>
2021-10-25 02:39:47 -0300

    Adding buggy code of remove vowels challenge

---
+++ 05-remove-vowels/main.py
@@ -0,0 +1,10 @@
+# This Python program remove all the vowels of the text passed 
+# by argument to the program. Idea based on the hebrew language.
+
+
+def run():
+    pass
+
+
+if __name__ == '__main__':
+    run()
