d5666cd6a5415303cc433f66715ab8283969fa6e
Pascal Salvador <smv7.github@gmail.com>
2021-10-27 15:55:37 -0300

    Half game with buggy result messages

--- 07-cachipun/main.py
+++ 07-cachipun/main.py
@@ -1,26 +1,70 @@
-# This program imitates the game rock, paper or scissors.
+# This program imitates the game "Rock, paper or scissors".
+
 import random
+from modules import get_string_choice, compare_choices
 
 
 def run():
-    welcome_message = open('./program_messages/welcome.txt', 'r')
+    messages_path = './program_messages/'
+    welcome_message = open(messages_path + 'welcome.txt', 'r')
     print(welcome_message.read())
 
     game_messages = {
-        'user_win': open(, 'r'),
-        'user_lose': open(, 'r'),
-        'user_victory': open(, 'r'),
-        'user_gameover': open(, 'r')
+        'welcome': open(messages_path + 'welcome.txt', 'r'),
+        'tie': open(messages_path + 'tie.txt', 'r'),
+        'round_result': open(messages_path + 'round_result.txt', 'r'),
+        'user_victory': open(messages_path + 'user_victory.txt', 'r'),
+        'user_gameover': open(messages_path + 'user_gameover.txt', 'r')
+    }
+
+    score = {
+        'user': 0,
+        'machine': 0
     }
 
     while True:
-        machine_choice = random.randint(2)
-        user_choice = input('1: Rock\n2: Paper\n3: Scissors')
+        machine_choice = random.randint(1,3)
+        user_choice = input('1: Rock\n2: Paper\n3: Scissors\n')
+        
         try:
             user_choice = int(user_choice)
         except:
             print('Please enter a valid number')
             continue
+        
+        user_string_choice = get_string_choice(user_choice)
+        machine_string_choice = get_string_choice(machine_choice)
+
+        if compare_choices(user_choice, machine_choice) == 'tie':
+            print(game_messages['tie']
+            .read()
+            .format(user_choice=user_string_choice,
+            machine_choice=machine_string_choice))
+        elif compare_choices(user_choice, machine_choice) == 'user':
+            score['user'] += 1
+            print(game_messages['round_result']
+            .read()
+            .format(result_message="¡Well played!",
+            user_choice=user_string_choice,
+            machine_choice=machine_string_choice,
+            user_score=score['user'],
+            machine_score=score['machine']))
+        else:
+            score['machine'] += 1
+            print(game_messages['round_result']
+            .read()
+            .format(result_message="Epic fail",
+            user_choice=user_string_choice,
+            machine_choice=machine_string_choice,
+            user_score=score['user'],
+            machine_score=score['machine']))
+
+        if score['user'] == 3:
+            print(game_messages['user_victory'].read())
+            return
+        elif score['machine'] == 3:
+            print(game_messages['user_gameover'].read())
+            return
 
 
 if __name__ == '__main__':
--- 07-cachipun/modules.py
+++ 07-cachipun/modules.py
@@ -1,22 +1,27 @@
 
 
-def machine_choice(random_number):
-    if random_number == 0:
+def get_string_choice(number):
+    '''This function takes a number between 1 and 3 and returns his equivalent in the game "Rock, paper or scissors"'''
+
+    if number == 1:
         return 'Rock'
-    elif random_number == 1:
+    elif number == 2:
         return 'Paper'
-    elif random_number == 2:
+    elif number == 3:
         return 'Scissors'
 
 
-def compare_choices(user_choice, machine_choice, game_messages_dict=None):
+def compare_choices(user_choice, machine_choice):
+    '''This function recieve 2 numbers and using "1=Rock, 2=Paper, 3=Scissors" returns the winner'''
+    
     if user_choice == machine_choice:
-        pass
+        return 'tie'
     elif user_choice == 1 and machine_choice == 3: # Rock beat Scissors
-        pass
+        return 'user'
     elif user_choice == 2 and machine_choice == 1: # Paper beat Rock
-        pass
+        return 'user'
     elif user_choice == 3 and machine_choice == 2: # Scissors beat Paper
-        pass
+        return 'user'
     else:
         return 'machine'
+
